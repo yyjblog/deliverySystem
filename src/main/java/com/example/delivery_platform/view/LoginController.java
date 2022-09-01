cdf9b788103b366fa198c44b097cb9db824b75b7
Mopemop <1452544248@qq.com>
2022-09-01 10:29:32 +0800

    修改资源文件位置

--- src/main/java/com/example/delivery_platform/view/LoginController.java
+++ src/main/java/com/example/delivery_platform/view/LoginController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
